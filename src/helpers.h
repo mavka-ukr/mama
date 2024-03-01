18538931749a30b48fecd78e38b2869da36f76d2
Davyd Kohut <davyd.kohut@gmail.com>
2024-03-01 02:32:59 +0200

    🧬

--- src/helpers.h
+++ src/helpers.h
@@ -1,8 +1,6 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-std::string cell_to_string(MaCell cell, int depth = 0);
-
 void print_instruction_with_index(MaCode* code,
                                   int index,
                                   MaInstruction instruction);
